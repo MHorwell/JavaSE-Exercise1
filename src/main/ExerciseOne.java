e9834a5af1b56490cd1b01886f7b7b33b83ed020
MHorwell <mattjhorwell@gmail.com>
2018-11-20 10:27:41 +0000

    Exercise 1 from the Java Exercise Book Completed

---
+++ src/main/ExerciseOne.java
@@ -0,0 +1,52 @@
+package main;
+
+public class ExerciseOne {
+	
+	public static void main(String[] args) {
+		
+		String helloWorld = "Hello World!";
+		System.out.println(helloWorld);
+		printWord("Matt");
+		System.out.println(helloWorldMethod());
+		System.out.println(sumOrMultiply(2, 4, true));
+		System.out.println(sumOrMultiply(0, 4, false));
+		
+		int [] arrayint = new int[10];
+		int j = 0;
+		for (int a = 0; a < arrayint.length; a++) {
+			arrayint[a] = j;
+			j += 5; {
+			System.out.println(sumOrMultiply(arrayint[a], 0, false));
+			}
+		}
+		
+	}
+	
+	public static void printWord(String word) {
+		System.out.println(word);
+	}
+	
+	public static String helloWorldMethod() {
+		return "Hello World!";
+	}
+	
+	
+	public static int sumOrMultiply(int numOne, int numTwo, boolean sumNumbers) {
+		if (numOne == 0 && numTwo != 0){
+			return (numTwo);
+		}
+		else if (numTwo == 0 && numOne != 0){
+			return (numOne);
+		}
+		else if(sumNumbers == true) {
+				return numOne + numTwo;
+		} else {
+				return numOne * numTwo;
+		}
+		
+	}
+	
+	
+	
+
+}
